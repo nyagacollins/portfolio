36b467df8dcc9be6f83b2a53a9b540b19219e6bb
nyagacollins <ndegecollins20@gmail.com>
2026-03-06 11:57:51 +0300

    Fix: Update ThemeProvider types for next-themes 0.4.4

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -159,7 +159,7 @@ const Projects = () => {
             className="text-center mt-12"
           >
             <motion.a
-              href="https://github.com/johndoe"
+              href="https://github.com/nyagacollins"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}
